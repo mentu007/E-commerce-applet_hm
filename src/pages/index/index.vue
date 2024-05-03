9094ddc05c908bb993c4a451597da7f7a8f0793c
Mentu <2621082160@qq.com>
2024-05-04 00:03:52 +0800

    feat: 首页分类组件静态效果

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { ref } from 'vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -22,8 +23,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
