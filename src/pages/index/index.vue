0570b546bbd519c76aa64896bd2155ac024f5ae7
Mentu <2621082160@qq.com>
2024-05-05 13:54:30 +0800

    feat: 猜你喜欢+顶部导航栏视图位置固定

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { ref } from 'vue'
 import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './components/HotPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -13,28 +14,50 @@ const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+//获取分类数据
 const categoryList = ref<CategoryItem[]>([])
 const getHomeCategoryaData = async () => {
   const res = await getHomeCategoryAPI()
   categoryList.value = res.result
 }
 
+//获取热门推荐数据
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 //页面加载
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryaData()
+  getHomeHotData()
 })
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper :list="bannerList" />
-  <!-- 分类面板 -->
-  <CategoryPanel :list="categoryList" />
+  <scroll-view class="scroll-view" scroll-y>
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推进 -->
+    <HotPanel :list="hotList" />
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
 }
 </style>
